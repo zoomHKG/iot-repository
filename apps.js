0e56c1e89e59beef1cc18e2cf94b1c6d2c43dbf0
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-13 09:11:27 +0200

    added repository file

---
+++ apps.js
@@ -0,0 +1,14 @@
+
+const apps = {
+  "test": {
+    "device": "esp8266",
+    "version": "v0.0.1",
+    "url": "https://github.com/rpidanny/assets/raw/master/iot-apps/blink-esp8266.bin",
+    "description": "Blink!"
+  }
+  /*
+  * please don"t add your app records down here!
+  * insert them in alphabetical order to help reduce merge conflicts.
+  * <3
+  */
+}
